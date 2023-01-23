8bf4c4001048d0ca1b96c56d40b748f155f0f59c
colehardy <cole0hardy@gmail.com>
2023-01-23 15:17:06 -0700

    Created the start for the print board mehtod

--- Mission3Group/supporting.cs
+++ Mission3Group/supporting.cs
@@ -8,6 +8,24 @@ namespace Mission3Group
 {
     class supporting
     {
-        // supporting class code goes here
+        private char[,] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+
+        public supporting( char[,] userBoard)
+        {
+            board = userBoard;
+        }
+
+        public void printBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j]);
+                }
+            }
+        }
+
+        // Method to return the winner if there is one
     }
 }
