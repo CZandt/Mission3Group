867df4ddb2732be6bbac44b519052dd1b5d3d48c
colehardy <cole0hardy@gmail.com>
2023-01-23 16:57:15 -0700

    Added notes for determinates and finished printing the board

--- Mission3Group/supporting.cs
+++ Mission3Group/supporting.cs
@@ -23,9 +23,24 @@ namespace Mission3Group
                 {
                     Console.Write(board[i, j]);
                 }
+                Console.Write("\n");
             }
         }
 
         // Method to return the winner if there is one
+
+        public int[] findWinner()
+        {
+            // find winners
+
+            int[] determinates = { 0, 0 };
+            
+            // if there is a winner index 0 becomes 1
+
+            // if index[0] is 1 then index 2 determines who is the winner 1 = player1 (X) , 2 = player2 (O)
+
+
+            return determinates;
+        }
     }
 }
