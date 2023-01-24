3a323d7873a4fcdba485b2a9383806bf43d1d082
JDMestanzy <jd.mestanza.rod@gmail.com>
2023-01-23 21:37:11 -0700

    Added the verification to see who won and if someone actually won

--- Mission3Group/supporting.cs
+++ Mission3Group/supporting.cs
@@ -8,9 +8,9 @@ namespace Mission3Group
 {
     class supporting
     {
-        private char[,] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+        private int[,] board = { { 0,0,0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-        public supporting( char[,] userBoard)
+        public supporting(int[,] userBoard)
         {
             board = userBoard;
         }
@@ -31,16 +31,59 @@ namespace Mission3Group
 
         public int[] findWinner()
         {
-            // find winners
-
-            int[] determinates = { 0, 0 };
             
+            int[] determinates = { 0, 0 };
+
             // if there is a winner index 0 becomes 1
 
             // if index[0] is 1 then index 2 determines who is the winner 1 = player1 (X) , 2 = player2 (O)
 
+            // check board rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != 0)
+                {
+                    determinates[0] = 1;
+                    determinates[1] = board[i, 0];
+                    break;
+                }
+            }
 
+            // check columns
+            if (determinates[0] == 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != 0)
+                    {
+                        determinates[0] = 1;
+                        determinates[1] = board[0, i];
+                        break;
+                    }
+                }
+            }
+
+            // check diagonals
+            if (determinates[0] == 0)
+            {
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != 0)
+                {
+                    determinates[0] = 1;
+                    determinates[1] = board[0, 0];
+                }
+                else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != 0)
+                {
+                    determinates[0] = 1;
+                    determinates[1] = board[0, 2];
+                }
+            }
+            if (determinates[0] == 1)
+            {
+                Console.WriteLine("Player " + determinates[1] + " won!");
+                
+            }
             return determinates;
         }
     }
 }
+
